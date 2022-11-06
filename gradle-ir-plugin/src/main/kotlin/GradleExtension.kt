d6ecc8411ec8977ecea152b9ffce7ce4ed57ae37
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-11-06 12:45:02 +0100

    feat(gradle-plugin): created classes used as brigde between gradle and the kotlin compiler

---
+++ gradle-ir-plugin/src/main/kotlin/GradleExtension.kt
@@ -0,0 +1,8 @@
+import org.gradle.api.file.RegularFileProperty
+import org.gradle.api.model.ObjectFactory
+import org.gradle.api.provider.Property
+
+open class GradleExtension(objects: ObjectFactory) {
+    val stringProperty: Property<String> = objects.property(String::class.java)
+    val fileProperty: RegularFileProperty = objects.fileProperty()
+}
