5d74cb602a3e867204ffd610d7edf9b4f552a226
ElisaTronetti <elisa.tronetti@studio.unibo.it>
2022-11-15 11:37:43 +0100

    build: included projects and added dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,12 @@
 buildscript {
-    extra["kotlin_plugin_id"] = "com.bnorm.template.kotlin-ir-plugin"
+    extra["kotlin_plugin_id"] = "com.etronetti.compiler-plugin-kmp"
 }
 
+@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     kotlin("jvm") version "1.7.10" apply false
-    id("org.jetbrains.dokka") version "1.7.10" apply false
-    id("com.gradle.plugin-publish") version "1.0.0" apply false
+    id("com.github.gmazzo.buildconfig") version "3.1.0" apply false
+    kotlin("multiplatform") version "1.7.10" apply false
 }
 
 allprojects {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,24 +1,4 @@
-@file:Suppress("UnstableApiUsage")
-
-pluginManagement {
-    repositories {
-        gradlePluginPortal()
-        google()
-        mavenCentral()
-    }
-}
-
-plugins {
-    id("com.gradle.enterprise") version "3.11.1"
-    id("org.danilopianini.gradle-pre-commit-git-hooks") version "1.0.20"
-}
-
-gitHooks {
-    commitMsg { conventionalCommits() }
-    createHooks()
-}
-
 rootProject.name = "compiler-plugin-kmp".toLowerCase()
-include(":kmp-tester")
+
 include(":gradle-ir-plugin")
 include(":compiler-ir-plugin")
